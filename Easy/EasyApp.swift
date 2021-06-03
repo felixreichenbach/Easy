aeff1fe3aa04987abdf33c1f94cf234f24c4995e
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2021-06-03 19:28:52 +0200

    Initial Commit

---
+++ Easy/EasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyApp.swift
+//  Easy
+//
+//  Created by Felix Reichenbach on 03.06.21.
+//
+
+import SwiftUI
+
+@main
+struct EasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
